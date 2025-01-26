3e6c9222a28d155c3fa3349a5490c38e05cc0ee7
opticalxhub <opticalxhub@gmail.com>
2025-01-26 22:53:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Monitoring Qatar Airways", "Handling your tickets", "Flying to 100 members"];
+const statusMessages = ["Monitoring Qatar Airways", "Handling your tickets", "Flying to 100 members", "Moderating QAV" ];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
